39955181724304d4cb33a377751ba276c179988a
Anthony Landreth <anthony.w.landreth@gmail.com>
2020-01-22 12:57:50 -0500

    Task FP.5
    The method receive should use std::unique_lock<std::mutex> and _condition.wait() to wait for and receive new messages and pull them from the queue using move semantics. The received object should then be returned by the receive function. Then, add the implementation of the method waitForGreen, in which an infinite while-loop runs and repeatedly calls the receive function on the message queue. Once it receives TrafficLightPhase::green, the method returns.

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -11,6 +11,13 @@ T MessageQueue<T>::receive()
     // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait() 
     // to wait for and receive new messages and pull them from the queue using move semantics. 
     // The received object should then be returned by the receive function. 
+    std::unique_lock<std::mutex> uLock(_mutex);
+    _cond.wait(uLock, [this] { return !_queue.empty(); }); // pass unique lock to condition variable
+    //remove last vector element from queue
+    T msg = std::move(_queue.back());
+    _queue.pop_back();
+
+    return msg;
 }
 
 template <typename T>
@@ -44,6 +51,11 @@ void TrafficLight::waitForGreen()
     // FP.5b : add the implementation of the method waitForGreen, in which an infinite while-loop 
     // runs and repeatedly calls the receive function on the message queue. 
     // Once it receives TrafficLightPhase::green, the method returns.
+		while(true) {
+      std::this_thread::sleep_for(std::chrono::milliseconds(1));
+			enum TrafficLightPhase phase = _queue->receive();
+			if(phase == TrafficLightPhase::green) { break; }
+		}
 }
 
 TrafficLightPhase TrafficLight::getCurrentPhase()
